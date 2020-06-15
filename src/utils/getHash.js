298946f708ac878e85ffa8fa465ada6acb3b48f2
dignelidxdx <digneli14@gmail.com>
2020-06-15 16:15:34 -0300

    completando estilos y mejorando eficiencia de la API en characters, haciendo llamados.

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,3 +1,8 @@
-const getHash = () => location.hash.slide(1).toLocateLowerCase().split('/')[1] || '/';
+// se parseo los elementos con slide y lo siguiente
+// se tendria #/1/ si no se parsea o si se deja hasta hash/
+// eslint-disable-next-line no-restricted-globals
+const getHash = () => 
+  // eslint-disable-next-line no-restricted-globals
+  location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
 
 export default getHash;
